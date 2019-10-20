9a0904777746d306ce826d1cac50a9c9218f76ed
Le Duy Anh <lenduya@gmail.com>
2019-10-20 17:37:41 +0200

    Add playerView to YTAct

--- app/src/main/java/com/example/lenduya/youtubeapp/YoutubeActivity.kt
+++ app/src/main/java/com/example/lenduya/youtubeapp/YoutubeActivity.kt
@@ -2,15 +2,28 @@ package com.example.lenduya.youtubeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
+        setContentView(layout)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        layout.addView(playerView)
     }
 
     override fun onInitializationSuccess(
